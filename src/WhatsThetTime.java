1d48bc5f305b740a5c5bb09ccab791a727164dc3
vukkt <63306506+vukkt@users.noreply.github.com>
2021-10-21 19:08:12 +0200

    Create WhatsThetTime.java

---
+++ src/WhatsThetTime.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class WhatsThetTime {
+
+	public static void main(String[] args) {
+		Date today = new Date();
+		String.format("%tc", new Date());
+		System.out.println(today);
+	}
+
+}
