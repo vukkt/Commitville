36d3184d6734ca61561f8f06dab329d18a487722
vukkt <63306506+vukkt@users.noreply.github.com>
2021-10-23 16:01:12 +0200

    Update SimpleGui1.java

--- src/guigui/SimpleGui1.java
+++ src/guigui/SimpleGui1.java
@@ -1,13 +1,16 @@
 package guigui;
 
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
+//** we implement the ActionListener interface to work with the events
 public class SimpleGui1 implements ActionListener {
 	JButton button;
+	JButton button2;
 
 	public static void main(String[] args) {
 		SimpleGui1 gui = new SimpleGui1();
@@ -17,11 +20,12 @@ public class SimpleGui1 implements ActionListener {
 	public void go() {
 		JFrame frame = new JFrame();
 		button = new JButton("click me");
-
+		button2 = new JButton("don't click me");
 		button.addActionListener(this);
+		// button2.addActionListener(this);
 
-		frame.getContentPane().add(button);
-
+		frame.getContentPane().add(BorderLayout.CENTER, button);
+		frame.getContentPane().add(BorderLayout.SOUTH, button2);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		frame.setSize(100, 100);
@@ -30,7 +34,9 @@ public class SimpleGui1 implements ActionListener {
 
 	}
 
+//** event handling method 
 	public void actionPerformed(ActionEvent event) {
 		button.setText("i've been clicked");
+		// button2.setText("i am worth nothing");
 	}
 }
