89db134b12ba71e5bf60ddb564f00b176e0061df
vukkt <63306506+vukkt@users.noreply.github.com>
2021-10-31 10:42:31 +0100

    update

--- src/midiplayerv3/MiniMusicPlayer3.java
+++ src/midiplayerv3/MiniMusicPlayer3.java
@@ -1,48 +1,52 @@
 package midiplayerv3;
 
-import javax.sound.midi.*;
-import java.io.*;
-import javax.swing.*;
-import java.awt.*;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Sequence;
+import javax.sound.midi.Sequencer;
+import javax.sound.midi.Track;
+import javax.swing.JFrame;
 
 public class MiniMusicPlayer3 {
 	static JFrame f = new JFrame("My First Music Video");
-	static MyDrawPanel ml;
+	// static MyDrawPanel ml;
 
 	public static void main(String[] args) {
 		MiniMusicPlayer3 mini = new MiniMusicPlayer3();
+
 		mini.go();
+
 	}
 
 	public void setUpGui() {
-		ml = new MyDrawPanel();
-		f.setContentPane(ml);
+		// ml = new MyDrawPanel();
+		// f.setContentPane(ml);
 		f.setBounds(30, 30, 300, 300);
 		f.setVisible(true);
 	}
 
 	public void go() {
 		setUpGui();
-		
+
 		try {
 			Sequencer sequencer = MidiSystem.getSequencer();
 			sequencer.open();
-			sequencer.addControllerEventListener(ml, new int[] {127});
+			// sequencer.addControllerEventListener(ml, new int[] {127});
 			Sequence seq = new Sequence(Sequence.PPQ, 4);
 			Track track = seq.createTrack();
-			
+
 			int r = 0;
-			for (int i=0; i<60; i+=4) {
-				r = (int) ((Math.random()*50)+1);
-				track.add(makeEvent(144,1,r,100,i));
-				track.add(makeEvent(176,1,127,9,i));
-				track.add(makeEvent(128,1,r,100,i+2));
+			for (int i = 0; i < 60; i += 4) {
+				r = (int) ((Math.random() * 50) + 1);
+				// track.add(makeEvent(144,1,r,100,i));
+				// track.add(makeEvent(176,1,127,9,i));
+				// track.add(makeEvent(128,1,r,100,i+2));
 			}
 			sequencer.setSequence(seq);
 			sequencer.start();
 			sequencer.setTempoInBPM(120);
-			
-			} catch (Exception ex) {ex.printStackTrace();}
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
 		}
 	}
 }
